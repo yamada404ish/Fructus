fd6d4a8e9149d6446e71a903c8f1c32d314a43c9
ryo <ryoukaijuu@gmail.com>
2025-07-15 23:42:20 +0800

    resize the icons and create the scan icon

--- app/src/main/java/com/example/fructus/ui/screens/notification/NotificationScreen.kt
+++ app/src/main/java/com/example/fructus/ui/screens/notification/NotificationScreen.kt
@@ -1,9 +1,46 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.fructus.ui.screens.notification
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.fructus.ui.theme.FructusTheme
 
 @Composable
 fun NotificationScreen(modifier: Modifier = Modifier) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                modifier = Modifier
+                    .padding(top = 50.dp),
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back"
+                    )
+                },
+                title = {}
+            )
+        }
+    ){}
+}
+
+@Preview
+@Composable
+private fun NotificationScreenPrev() {
+    FructusTheme {
+        NotificationScreen()
+
+    }
 
 }
