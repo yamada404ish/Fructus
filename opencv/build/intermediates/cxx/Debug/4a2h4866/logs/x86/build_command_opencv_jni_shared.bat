bccc3d5f2a9b18bd8955a060d341b1221ce1a71e
ryo <ryoukaijuu@gmail.com>
2025-08-18 17:35:29 +0800

    Clean repo: remove build and IDE files from Git history

--- opencv/build/intermediates/cxx/Debug/4a2h4866/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Jayson\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Jayson\\Desktop\\Fructus2\\opencv\\.cxx\\Debug\\4a2h4866\\x86" ^
-  opencv_jni_shared
