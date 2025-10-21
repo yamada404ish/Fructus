93bbbdc51e1802f463e112917c913faab5a93efe
yamada <yamada404ish@gmail.com>
2025-10-21 16:40:47 +0800

    recipe tabs

--- app/src/main/java/com/example/fructus/ui/detail/components/RecipeInformation.kt
+++ app/src/main/java/com/example/fructus/ui/detail/components/RecipeInformation.kt
@@ -2,6 +2,8 @@ package com.example.fructus.ui.detail.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,7 +23,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,7 +35,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.fructus.data.Recipe
 import com.example.fructus.ui.shared.ScreenTopBar
 import com.example.fructus.ui.theme.appColors
 import com.example.fructus.ui.theme.poppinsFontFamily
@@ -54,6 +58,7 @@ fun RecipeInformation(
         context.loadRecipesFromJson().find { it.name.equals(name, ignoreCase = true) }
     }
 
+    var selectedTab by remember { mutableStateOf("Ingredients") }
 
 
     Scaffold(
@@ -115,80 +120,119 @@ fun RecipeInformation(
                 }
             }
             Spacer(modifier = Modifier.height(24.dp))
+
+            Row (
+                modifier = Modifier
+                    .padding(bottom = 6.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Ingredients",
+                        fontFamily = poppinsFontFamily,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp,
+                        color = if (selectedTab == "Ingredients") colors.textPrimary else colors.textSecondary,
+                        modifier = Modifier
+                            .clickable (
+                                onClick = { selectedTab = "Ingredients"},
+                                indication = null,
+                                interactionSource = remember { MutableInteractionSource() }
+                            )
+                            .background(
+                                if (selectedTab == "Ingredients") colors.innerBox else Color.Transparent,
+                                RoundedCornerShape(8.dp)
+                            )
+                            .padding(horizontal = 12.dp, vertical = 6.dp)
+                    )
+                }
+
+                Spacer (modifier = Modifier.width(20.dp))
+
+                Text(
+                    text = "Preparations",
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = if (selectedTab == "Preparations") colors.textPrimary else colors.textSecondary,
+                    modifier = Modifier
+                        .clickable (
+                            onClick = { selectedTab = "Preparations"},
+                            indication = null,
+                            interactionSource = remember { MutableInteractionSource() }
+                        )
+                        .background(
+                            if (selectedTab == "Preparations") colors.ingr.copy(alpha = 0.2f) else Color.Transparent,
+                            RoundedCornerShape(8.dp)
+                        )
+                        .padding(horizontal = 12.dp, vertical = 6.dp)
+                )
+            }
             Column (
                 modifier = Modifier
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
             ) {
-                Text(
-                    text = "Ingredients",
-                    fontFamily = poppinsFontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
-                    color = colors.textPrimary,
-                )
-
-                Column (
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                ){
-                    recipe?.ingredients?.forEach { ingredient ->
-                        Text(
-                            text = "● $ingredient",
-                            fontFamily = poppinsFontFamily,
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 14.sp,
-                            color = colors.textPrimary
-                        )
-                        HorizontalLine()
-                        Spacer(modifier = Modifier.height(10.dp))
-                    }
-                }
-                Spacer(modifier = Modifier.height(24.dp))
-                Text(
-                    text = "Preparations",
-                    fontFamily = poppinsFontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
-                    color = colors.textPrimary,
-                )
-                Column (
-                    modifier = Modifier
-                        .padding(top = 16.dp)
-                ){
-                    recipe?.directions?.forEachIndexed { index, step ->
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Box(
-                                modifier = Modifier
-                                    .size(40.dp)
-                                    .background(colors.ingr, shape = RoundedCornerShape(8.dp)),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Text(
-                                    text = "${index + 1}",
-                                    fontFamily = poppinsFontFamily,
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 14.sp,
-                                    color = Color(0xFF000000)
-                                )
-                            }
-                            Spacer(modifier = Modifier.width(10.dp))
+                if (selectedTab == "Ingredients") {
+                    Column (
+                        modifier = Modifier
+                            .padding(top = 16.dp, start = 6.dp, end = 6.dp)
+                    ){
+                        recipe?.ingredients?.forEach { ingredient ->
                             Text(
-                                text = step,
+                                text = "● $ingredient",
                                 fontFamily = poppinsFontFamily,
                                 fontWeight = FontWeight.Normal,
                                 fontSize = 14.sp,
                                 color = colors.textPrimary
                             )
+                            HorizontalLine()
+                            Spacer(modifier = Modifier.height(10.dp))
                         }
-                        Spacer(modifier = Modifier.height(14.dp))
+                    }
+                } else {
+                    Column (
+                        modifier = Modifier
+                            .padding(top = 16.dp, start = 6.dp, end = 6.dp)
+                    ){
+                        recipe?.directions?.forEachIndexed { index, step ->
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Box(
+                                    modifier = Modifier
+                                        .size(40.dp)
+                                        .background(colors.ingr, shape = RoundedCornerShape(8.dp)),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = "${index + 1}",
+                                        fontFamily = poppinsFontFamily,
+                                        fontWeight = FontWeight.Bold,
+                                        fontSize = 14.sp,
+                                        color = Color(0xFF000000)
+                                    )
+                                }
+                                Spacer(modifier = Modifier.width(10.dp))
+                                Text(
+                                    text = step,
+                                    fontFamily = poppinsFontFamily,
+                                    fontWeight = FontWeight.Normal,
+                                    fontSize = 14.sp,
+                                    color = colors.textPrimary
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(14.dp))
+                        }
                     }
                 }
+                Spacer(modifier = Modifier.height(24.dp))
             }
-
         }
     }
 }
