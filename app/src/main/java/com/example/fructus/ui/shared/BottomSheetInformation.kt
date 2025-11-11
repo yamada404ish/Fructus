517f89cf3a99131c5e1c526288f83adcc0a5fccc
yamada <yamada404ish@gmail.com>
2025-11-11 23:34:36 +0800

    Enable scrolling in BottomSheet

--- app/src/main/java/com/example/fructus/ui/shared/BottomSheetInformation.kt
+++ app/src/main/java/com/example/fructus/ui/shared/BottomSheetInformation.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
@@ -100,6 +102,7 @@ fun CustomBottomSheet(
                     modifier = Modifier
                         .weight(1f)
                         .padding(bottom = 16.dp)
+                        .verticalScroll(rememberScrollState())
                 ) {
                     FruitAnalysis(
                         fruitName = fruitName,
