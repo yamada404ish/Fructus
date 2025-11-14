6ba228da756743ad3632af9468164ebc2c77cb72
yamada <yamada404ish@gmail.com>
2025-11-15 00:51:14 +0800

    Refactor Tips component and adjust UI

--- app/src/main/java/com/example/fructus/ui/camera/components/Tips.kt
+++ app/src/main/java/com/example/fructus/ui/camera/components/Tips.kt
@@ -1,10 +1,13 @@
 package com.example.fructus.ui.camera.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,33 +20,40 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fructus.R
-import com.example.fructus.ui.shared.InfoCard
+import com.example.fructus.ui.detail.components.HorizontalLine
 import com.example.fructus.ui.theme.FructusTheme
 import com.example.fructus.ui.theme.appColors
 import com.example.fructus.ui.theme.poppinsFontFamily
 
 @Composable
 fun Tips(modifier: Modifier = Modifier) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.Top
+    Column (
+        verticalArrangement = Arrangement.Center
     ) {
-        InfoCard(icon = R.drawable.ic_temp, title = "Keep Refrigerated", subtitle = "Store fruit in cool conditions")
-        InfoCard(icon = R.drawable.ic_sun, title = "Avoid Sunlight", subtitle = "Keep fruits away from the sunlight")
-        InfoCard(icon = R.drawable.ic_faucet,title = "Wash and Dry", subtitle = "To prevent bacteria growth")
+        InfoCard(icon = R.drawable.ic_temp, title = "Keep Cool", subtitle = "Store fruit in cool " +
+                "conditions", subtext = "Most fresh fruits last longer if stored in a cool place")
+        InfoCard(icon = R.drawable.ic_sun, title = "Avoid Sunlight", subtitle = "Keep fruits away" +
+                " from the sunlight", subtext = "Heat causes fruits to lose their moisture, so it is best to keep your fruits away from sunlight.")
+        InfoCard(icon = R.drawable.ic_faucet,title = "Wash and Dry", subtitle = "To prevent " +
+                "bacteria growth", subtext = "Fruits may arrive contaminated with bacteria, it is" +
+                " best to wash them before storing or consuming.")
     }
 }
 
@@ -53,54 +63,110 @@ fun InfoCard(
     icon: Int,
     title: String,
     subtitle: String,
+    subtext: String,
     modifier: Modifier = Modifier
 ) {
 
     val colors = MaterialTheme.appColors
+    var expandedState by remember { mutableStateOf(false) }
+    val rotationState by animateFloatAsState (
+        targetValue = if (expandedState) 180f else 0f
+    )
 
     Card(
         modifier = modifier
-            .height(110.dp)
-            .width(100.dp)
+//            .height(110.dp)
+            .fillMaxWidth()
+            .animateContentSize(
+                animationSpec = tween (
+                    durationMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+            )
             .clip(RoundedCornerShape(20.dp))
             .shadow(8.dp, RoundedCornerShape(2.dp)),
         colors = CardDefaults.cardColors(containerColor = colors.tip),
-        elevation = CardDefaults.cardElevation(8.dp)
+        elevation = CardDefaults.cardElevation(8.dp),
+        onClick = {
+            expandedState = !expandedState
+        }
     ) {
-        Column(
+        Row (
             modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             Icon(
                 painter = painterResource(icon),
                 contentDescription = "Tips icon",
                 modifier = Modifier
-                    .size(38.dp)
+                    .size(32.dp)
 
             )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 14.sp,
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Medium,
+                    color = colors.textPrimary,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = subtitle,
+                    fontSize = 10.sp,
+                    fontFamily = poppinsFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    color = colors.textPrimary,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 10.sp
+                )
+            }
+            Icon(
+                painter = painterResource(R.drawable.downdown),
+                contentDescription = "down icon",
+                modifier = Modifier
+                    .rotate(rotationState)
+                    .size(16.dp),
+                tint = colors.textTertiary
+            )
+        }
+        if (expandedState) {
+            HorizontalLine(
+                color = colors.bg
+            )
+            Text (
+                text = subtext,
+                fontFamily = poppinsFontFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 12.sp,
+                color = colors.textPrimary,
+                modifier =  (
+                Modifier
+                    .padding(start = 16.dp, end = 16.dp, top = 10.dp, bottom = 10.dp)
+                )
+            )
+            Spacer(modifier = Modifier.height(2.dp))
 
             Text(
-                text = title,
-                fontSize = 8.sp,
+                text = "Source: unlockfood.ca",
                 fontFamily = poppinsFontFamily,
-                fontWeight = FontWeight.Bold,
+                fontStyle = FontStyle.Italic,
+                fontSize = 10.sp,
                 color = colors.textPrimary,
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = subtitle,
-                fontSize = 8.sp,
-                fontFamily = poppinsFontFamily,
-                fontWeight = FontWeight.Medium,
-                color = colors.textPrimary,
-                textAlign = TextAlign.Center,
-                lineHeight = 10.sp
+                modifier =  (
+                Modifier
+                    .padding(start = 16.dp, end = 16.dp, bottom = 10.dp)
+                )
             )
         }
     }
+    Spacer(modifier = Modifier.height(8.dp))
 }
 
 
--- app/src/main/java/com/example/fructus/ui/detail/components/SuggestedRecipe.kt
+++ app/src/main/java/com/example/fructus/ui/detail/components/SuggestedRecipe.kt
@@ -24,19 +24,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color.Companion.Unspecified
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fructus.R
-import com.example.fructus.ui.theme.FructusTheme
 import com.example.fructus.ui.theme.appColors
 import com.example.fructus.ui.theme.poppinsFontFamily
-import com.example.fructus.util.getDrawableIdByName
 
 
 @Composable
@@ -67,10 +63,9 @@ fun SuggestedRecipe(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp),
+                .padding(horizontal = 16.dp, vertical = 12.dp),
             horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-            ,
+            verticalAlignment = Alignment.CenterVertically,
 
         ) {
             // Food image
